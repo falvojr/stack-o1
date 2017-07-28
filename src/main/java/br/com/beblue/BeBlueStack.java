86f468b6b6027e060c8898e4ef19d93da2d3ce0b
Venilton FalvoJr <falvojr@gmail.com>
2017-07-27 21:57:17 -0300

    Java Stack O(1)

--- src/main/java/br/com/beblue/BeBlueStack.java
+++ src/main/java/br/com/beblue/BeBlueStack.java
@@ -1,17 +1,17 @@
 package br.com.beblue;
+
 import java.util.Stack;
 
 /**
- * Como você implementaria uma classe de pilha de inteiros que possui os métodos
- * push, pop e min. Onde min retorna o menor inteiro na pilha e todos os métodos
- * são O(1).
+ * Como você implementaria uma classe de pilha de inteiros que possui os métodos push, pop e min. Onde min retorna o
+ * menor inteiro na pilha e todos os métodos são O(1).
  * 
  * @author falvojr
  */
 public class BeBlueStack {
 
 	private static final String MSG_EMPTY_STACK = "Empty stack!";
-	
+
 	private Stack<Integer> stack;
 	private Integer min;
 
@@ -24,8 +24,9 @@ public class BeBlueStack {
 		if (this.stack.isEmpty()) {
 			this.min = newItem;
 		} else if (newItem < this.min) {
-			// Se o novo item (push) for menor que o mínimo, o mínimo deve ser atualizado. 
-			// Antes isso, executamos uma equacão para o cálculo do valor de referência (que viabilizará as operacões em O(1)).
+			// Se o novo item (push) for menor que o mínimo, o mínimo deve ser atualizado.
+			// Antes isso, executamos uma equacão para o cálculo do valor de referência,
+			// que viabilizará as operacões em O(1).
 			topItem = this.evalMin(newItem, this.min);
 			this.min = newItem;
 		}
@@ -39,31 +40,32 @@ public class BeBlueStack {
 		final Integer topItem = this.stack.pop();
 		if (topItem < this.min) {
 			// Se o valor do pop (topo) for menor que o mínimo, então ele é o próprio mínimo (vide método evalMin).
-			// Com isso, usamos novamente a equacão para calcular o novo minimo (já que o atual foi removido). 
+			// Com isso, usamos novamente a equacão para calcular o novo minimo (já que o atual foi removido).
 			this.min = this.evalMin(this.min, topItem);
 		}
 	}
-	
+
 	public Integer min() {
 		if (this.stack.isEmpty()) {
 			throw new RuntimeException(MSG_EMPTY_STACK);
 		}
 		return this.min;
 	}
-	
-    public Integer peek() {
-        if (this.stack.isEmpty()) {
-        	throw new RuntimeException(MSG_EMPTY_STACK);
-        }
-        // Caso o item do pop (topo) for menor que o mínimo, então ele é o próprio mínimo. Senão o item é retornado normalmente.
-        final Integer topItem = this.stack.peek();
-        return topItem < this.min ? this.min : topItem;
-    }
 
-    /**
-     * Equacao generica para manipular a inclusão de elementos minimos na pilha.
-     * Essencial para para que o algoritmo seja O(1).
-     */
+	public Integer peek() {
+		if (this.stack.isEmpty()) {
+			throw new RuntimeException(MSG_EMPTY_STACK);
+		}
+		// Caso o item do pop (topo) for menor que o mínimo, então ele é o próprio mínimo. Senão o item é 
+		// retornado normalmente.
+		final Integer topItem = this.stack.peek();
+		return topItem < this.min ? this.min : topItem;
+	}
+
+	/**
+	 * Equacao generica para manipular a inclusão de elementos minimos na pilha. Essencial para para que o algoritmo
+	 * seja O(1).
+	 */
 	private int evalMin(Integer x, Integer min) {
 		return 2 * x - min;
 	}
